200dfbd07b5d3cbed5aa3589c17e605ded77419e
Sardorbekk <sardorbekimomaddinov@gmail.com>
2023-01-21 09:06:58 +0500

    Amalladim xudoga shukr

--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
+       int a=(int) symbol;
+       if (a>=65&&a<=90) System.out.println("English");
+       else if (a>=97&&a<=122) System.out.println("English");
+       else System.out.println("Non English");
     }
-}
+    }
+
